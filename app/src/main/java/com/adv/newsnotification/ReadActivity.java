bc35f7d2c14d860095e2ed69419317f911ec5d0c
DoopDip <narit.su@icloud.com>
2017-12-10 21:22:33 +0700

    Decorate ReadActivity

--- app/src/main/java/com/adv/newsnotification/ReadActivity.java
+++ app/src/main/java/com/adv/newsnotification/ReadActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.florent37.picassopalette.PicassoPalette;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -27,7 +28,17 @@ public class ReadActivity extends AppCompatActivity {
         textViewTitle.setText(news.get(0).title);
 
         ImageView imageView = (ImageView) findViewById(R.id.read_image);
-        Picasso.with(this).load(news.get(0).image).into(imageView);
+        Picasso.with(this).load(news.get(0).image).error(R.drawable.no_image).into(imageView);
+
+        Picasso.with(this).load(news.get(0).image).error(R.drawable.no_image).into(imageView,
+                PicassoPalette.with(news.get(0).image,imageView)
+                        .use(PicassoPalette.Profile.VIBRANT)
+                        .intoBackground(textViewTitle)
+                        .intoTextColor(textViewTitle)
+                        .use(PicassoPalette.Profile.VIBRANT_DARK)
+                        .intoBackground(textViewTitle, PicassoPalette.Swatch.RGB)
+                        .intoTextColor(textViewTitle, PicassoPalette.Swatch.BODY_TEXT_COLOR)
+        );
 
         TextView textViewContent = (TextView) findViewById(R.id.read_content);
         textViewContent.setText(news.get(0).content);
